a3b884099347041643c3518ea74dee3c7c1fd073
Herr Turing <admin@nda.software>
2023-10-14 15:49:20 -0300

    Initial commit.

---
+++ rollup.config.mjs
@@ -0,0 +1,42 @@
+import { existsSync, mkdirSync, writeFileSync } from 'fs'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
+import nodeResolve from '@rollup/plugin-node-resolve'
+import typescript from 'rollup-plugin-typescript2'
+import copy from 'rollup-plugin-copy'
+
+if (!existsSync('./.temp/dummy.js')) {
+    mkdirSync('./.temp/')
+
+    writeFileSync(
+        './.temp/dummy.js',
+        'export const dummy = () => console.log(42);'
+    )
+}
+
+export default [
+    {
+        input: 'src/index.ts',
+        output: [
+            {
+                exports: 'named',
+                preserveModules: true,
+                interop: 'auto',
+                dir: '.build/',
+                format: 'esm'
+            }
+        ],
+        plugins: [
+            typescript({
+                tsconfig: 'tsconfig.json',
+                outDir: '.build/',
+                include: ['./src/**/*.ts']
+            }),
+            nodeResolve(),
+            peerDepsExternal(),
+            copy({
+                targets: [{ src: './src/templates/', dest: './.build/' }]
+            })
+        ]
+    }
+]
