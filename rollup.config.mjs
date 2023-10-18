c2d624fc537c16619da70300db9ceecc8542c4c8
Herr Turing <admin@nda.software>
2023-10-18 14:20:26 -0300

    More cleanup of content not needed in this project.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,28 +1,15 @@
-import { existsSync, mkdirSync, writeFileSync } from "fs";
-
 import nodeResolve from "@rollup/plugin-node-resolve";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import typescript from "rollup-plugin-typescript2";
 import json from "rollup-plugin-json";
 
-if (!existsSync("./.temp/dummy.js")) {
-    mkdirSync("./.temp/");
-
-    writeFileSync(
-        "./.temp/dummy.js",
-        "export const dummy = () => console.log(42);"
-    );
-}
-
 export default [{
     input: "src/index.ts",
-    output: [
-        {
-            exports: "named",
-            dir: "dist/",
-            format: "cjs"
-        },
-    ],
+    output: [{
+        exports: "named",
+        dir: "dist/",
+        format: "cjs"
+    }],
     plugins: [
         typescript({ tsconfig: "tsconfig.json" }),
         nodeResolve(),
