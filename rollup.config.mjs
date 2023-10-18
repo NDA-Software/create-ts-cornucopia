f0485077f2b88f4211ca3fd3c1860052ca7d0b01
Herr Turing <admin@nda.software>
2023-10-18 15:38:50 -0300

    Added rollup-plugin-copy and templates folder.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import typescript from "rollup-plugin-typescript2";
 import json from "rollup-plugin-json";
+import copy from "rollup-plugin-copy";
 
 export default [{
     input: "src/index.ts",
@@ -15,5 +16,11 @@ export default [{
         nodeResolve(),
         peerDepsExternal(),
         json(),
+        copy({
+            targets: [
+                { src: 'src/templates/', dest: "dist/" }
+            ],
+            recursive: true
+        })
     ],
 }];
