3641c8c60fc7a44964f8647403876539dec914b9
Herr Turing <admin@nda.software>
2023-10-22 15:32:17 -0300

    Added testing.

---
+++ tests/index.test.ts
@@ -0,0 +1,42 @@
+import { spawn } from 'child_process';
+import { existsSync, mkdirSync, readdirSync } from 'fs';
+
+const testFolder = './.temp/test/';
+if (!existsSync(testFolder))
+    mkdirSync(testFolder, { recursive: true });
+
+const filesExpected = [
+    '.eslintrc.json',
+    '.github',
+    '.gitignore',
+    'jest.config.json',
+    'jest.config.mjs',
+    'package.json',
+    'rollup.config.mjs',
+    'tsconfig.json'
+];
+
+test('Testing file generations...', (done) => {
+    const app = spawn(`cd ${testFolder} && node ../../dist/index.cjs`, {
+        shell: true
+    });
+
+    app.stdout.on('data', function (data) {
+        console.log(data.toString());
+
+        app.stdin.write('\n');
+    });
+
+    app.stdout.on('end', () => {
+        const filesFound = readdirSync(testFolder);
+
+        try {
+            for (let i = 0; i < filesFound.length; i++)
+                expect(filesFound[i]).toBe(filesExpected[i]);
+        } catch (err) {
+            return done(err);
+        }
+
+        return done();
+    });
+});
